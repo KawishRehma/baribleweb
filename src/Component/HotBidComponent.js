8576d4c88d928a0060a0d1f58d80dbc4bb326c88
kawish <kawishr001@gmail.com>
2021-09-13 15:32:57 +0500

    today i am working on card to set the hower on small circle images and also set the tooltip on it

--- src/Component/HotBidComponent.js
+++ src/Component/HotBidComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Component/HotBidComponent.css";
-import { Card, Row, Col, Button} from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import { BiHeart } from "react-icons/bi";
 import { FiMoreHorizontal } from "react-icons/fi";
 
@@ -9,116 +9,126 @@ export const HotBidComponent = () => {
     <div className="Hotbid-one">
       <h3> Hot Bids </h3>
       <Row>
-        <Col>
-          <Card className="card-main" style={{ width: "16rem", height:"27rem"}}>
+        {/* <Col>
+          <Card
+            className="card-main"
+            style={{ width: "16rem", height: "27rem" }}
+          >
+            <div className="dotted-bar">
+              <img className="circle-img" src="assets/images/first.jpg" />
+              <img className="circle-img1" src="assets/images/front.jpg" />
 
-          <div className="dotted-bar">
-          
-          <img className="circle-img" src="assets/images/first.jpg"  />
-          
-          <img className="circle-img1"  src="assets/images/front.jpg"  /> 
-          
-          
- 
-          <Button className="btn2-dotted" variant="light">  <FiMoreHorizontal /> </Button>
-          
-          </div>
+              <Button className="btn2-dotted" variant="light">
+                {" "}
+                <FiMoreHorizontal />{" "}
+              </Button>
+            </div>
             <Card.Body>
-            
               <img src="assets/images/first.jpg" height="200" width="225" />
               <h4>Lucky Watcher</h4>
               <p className="card-p1"> From 3.4 ETH 15/90 </p>
               <div className="btn-para">
-              <p className="card-p2">Bid 2 wETH</p> 
-              <Button className="btn-heart" variant="light"> <BiHeart />  </Button> </div>
+                <p className="card-p2">Bid 2 wETH</p>
+                <Button className="btn-heart" variant="light">
+                  {" "}
+                  <BiHeart />{" "}
+                </Button>{" "}
+              </div>
             </Card.Body>
-           
           </Card>
         </Col>
 
         <Col>
-          <Card className="card-main" style={{ width: "16rem", height:"27rem"}}>
+          <Card
+            className="card-main"
+            style={{ width: "16rem", height: "27rem" }}
+          >
+            <div className="dotted-bar">
+              <img className="circle-img" src="assets/images/second.jpg" />
+              <img className="circle-img1" src="assets/images/fifth.jpg" />
 
-          <div className="dotted-bar">
-          
-          <img className="circle-img" src="assets/images/first.jpg"  />
-          
-          <img className="circle-img1"  src="assets/images/front.jpg"  /> 
-          
-          
- 
-          <Button className="btn2-dotted" variant="light">  <FiMoreHorizontal /> </Button>
-          
-          </div>
+              <Button className="btn2-dotted" variant="light">
+                {" "}
+                <FiMoreHorizontal />{" "}
+              </Button>
+            </div>
             <Card.Body>
-            
               <img src="assets/images/front.jpg" height="200" width="225" />
               <h4>The Hunter</h4>
               <p className="card-p1"> From 3.4 ETH 15/90 </p>
               <div className="btn-para">
-              <p className="card-p2">Bid 2 wETH</p> 
-              <Button className="btn-heart" variant="light"> <BiHeart />  </Button> </div>
+                <p className="card-p2">Bid 2 wETH</p>
+                <Button className="btn-heart" variant="light">
+                  {" "}
+                  <BiHeart />{" "}
+                </Button>{" "}
+              </div>
             </Card.Body>
-           
           </Card>
         </Col>
 
         <Col>
-          <Card className="card-main" style={{ width: "16rem", height:"27rem"}}>
+          <Card
+            className="card-main"
+            style={{ width: "16rem", height: "27rem" }}
+          >
+            <div className="dotted-bar">
+              <img className="circle-img" src="assets/images/first.jpg" />
+              <img className="circle-img1" src="assets/images/front.jpg" />
 
-          <div className="dotted-bar">
-          
-          <img className="circle-img" src="assets/images/first.jpg"  />
-          
-          <img className="circle-img1"  src="assets/images/front.jpg"  /> 
-          
-          
- 
-          <Button className="btn2-dotted" variant="light">  <FiMoreHorizontal /> </Button>
-          
-          </div>
+              <Button className="btn2-dotted" variant="light">
+                {" "}
+                <FiMoreHorizontal />{" "}
+              </Button>
+            </div>
             <Card.Body>
-            
               <img src="assets/images/fifth.jpg" height="200" width="225" />
               <h4>Night Watch</h4>
               <p className="card-p1"> From 3.4 ETH 15/90 </p>
               <div className="btn-para">
-              <p className="card-p2">Bid 2 wETH</p> 
-              <Button className="btn-heart" variant="light"> <BiHeart />  </Button> </div>
+                <p className="card-p2">Bid 2 wETH</p>
+                <Button className="btn-heart" variant="light">
+                  {" "}
+                  <BiHeart />{" "}
+                </Button>{" "}
+              </div>
             </Card.Body>
-           
           </Card>
-        </Col>
+        </Col> */}
 
         <Col>
-          <Card className="card-main" style={{ width: "16rem", height:"27rem"}}>
+          <Card
+            className="card-main"
+            style={{ width: "16rem", height: "27rem" }}
+          >
+            <div className="dotted-bar">
 
-          <div className="dotted-bar">
-          
-          <img className="circle-img" src="assets/images/first.jpg"  />
-          
-          <img className="circle-img1"  src="assets/images/front.jpg"  /> 
-         
-          
- 
-          <Button className="btn2-dotted" variant="light">  <FiMoreHorizontal /> </Button>
-          
-          </div>
+              <span className="something">
+              <img className="circle-img" src="assets/images/front.jpg" />
+                <span class="tooltiptext">Collection: Loot</span>
+                </span>
+              
+              <span className="something">
+                <img className="circle-img11" src="assets/images/fifth.jpg" />
+                <span class="tooltiptext">Creator: Lucere</span>
+                </span>
+              <Button className="btn2-dotted" variant="light">
+                <FiMoreHorizontal />
+              </Button>
+            </div>
             <Card.Body>
-            
-              <img src="assets/images/second.jpg" height="200" width="225" />
+              <img className="main-img" src="assets/images/second.jpg"  />
               <h4>Black Box</h4>
               <p className="card-p1"> From 3.4 ETH 15/90 </p>
               <div className="btn-para">
-              <p className="card-p2">Bid 2 wETH</p> 
-              <Button className="btn-heart" variant="light"> <BiHeart />  </Button> </div>
+                <p className="card-p2">Bid 2 wETH</p>
+                <Button className="btn-heart" variant="light">
+                  <BiHeart />
+                </Button>
+              </div>
             </Card.Body>
-           
           </Card>
         </Col>
-
-        
-        
       </Row>
     </div>
   );
