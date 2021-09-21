37eea83a1aa133bf85d8b2c44ef685ad9e9dae96
kawish <kawishr001@gmail.com>
2021-09-21 10:26:08 +0500

    both hover effect and tool tip text working as expected

--- src/Shared/Hotbiddata.js
+++ src/Shared/Hotbiddata.js
@@ -9,32 +9,35 @@ export const HOTBIDDATA = [
       image2: "assets/images/fifth.jpg",
      
     },
-    // {
-    //     id: 1,
-    //     name: "Black Box",
-    //     description: "From 3.4 ETH 15/90",
-    //     image: "assets/images/first.jpg",
-    //     image1: "assets/images/front.jpg",
-    //     image2: "assets/images/fifth.jpg",
+    {
+        id: 1,
+        name: "Black Box",
+        description: "From 3.4 ETH 15/90",
+        data: "Bid 2 wETH",
+        image: "assets/images/front.jpg",
+        image1: "assets/images/first.jpg",
+        image2: "assets/images/fifth.jpg",
        
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Black Box",
-    //     description: "From 3.4 ETH 15/90",
-    //     image: "assets/images/first.jpg",
-    //     image1: "assets/images/front.jpg",
-    //     image2: "assets/images/fifth.jpg",
+      },
+      {
+        id: 2,
+        name: "Black Box",
+        description: "From 3.4 ETH 15/90",
+        data: "Bid 2 wETH",
+        image: "assets/images/fifth.jpg",
+        image1: "assets/images/front.jpg",
+        image2: "assets/images/first.jpg",
        
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Black Box",
-    //     description: "From 3.4 ETH 15/90",
-    //     image: "assets/images/first.jpg",
-    //     image1: "assets/images/front.jpg",
-    //     image2: "assets/images/fifth.jpg",
+      },
+      {
+        id: 3,
+        name: "Black Box",
+        description: "From 3.4 ETH 15/90",
+        data: "Bid 2 wETH",
+        image: "assets/images/second.jpg",
+        image1: "assets/images/third.jpg",
+        image2: "assets/images/fifth.jpg",
        
-    //   },
+      },
        
   ];
