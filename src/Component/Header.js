58e46e80f4f79eedc2206bb100c9449040499d6e
kawish <kawishr001@gmail.com>
2021-09-24 17:23:29 +0500

    create the gradient button with hover color effects

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaFacebook, FaGooglePlusG, FaInstagram, FaTwitterSquare, FaYoutube, FaLinkedin, FaPinterest } from "react-icons/fa";
+
+import { Container, Row, Col } from "react-bootstrap";
 import {
   Navbar,
   Nav,
@@ -10,125 +11,130 @@ import {
 } from "react-bootstrap";
 import "../Component/Header.css";
 
-
 export const Header = () => {
   return (
-    <div className="main-nav">
-
-<Navbar bg="light" expand="lg" className="navbar-main11">
-  <Navbar.Brand href="#" className="nav-logo"><img src="assets/images/logo.png" height="40" width="48" alt="Barible" />
-          <b className="icon-name">Barible </b></Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Form className="d-flex">
-      <FormControl
-        type="search"
-       
-        placeholder=  "collection, item or user"
-        className="mr-2"
-        aria-label="Search"
-      />
-      
-    </Form>
-      <Nav.Link href="#action1"> <b>Explore </b> </Nav.Link>
-      <Nav.Link href="#action1"> <b>My items </b> </Nav.Link>
-      <Nav.Link href="#action1"> <b>Following </b> </Nav.Link>
-      <Nav.Link href="#action1"> <b>Activity </b> </Nav.Link>
-      <Nav.Link href="#action1"> <b>How it Works </b> </Nav.Link>
-      
-      <NavDropdown title="Community" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Rari Token</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Discussion</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Voting</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Suggest Feature</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Barible Protocol</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Subscribe</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5" className="fonts-main">  <FaGooglePlusG/> <FaFacebook />  <FaInstagram /> <FaTwitterSquare /> <FaYoutube /> <FaLinkedin/> <FaPinterest/>
-      </NavDropdown.Item>
-      </NavDropdown>
-      
-    </Nav>
-    <a class="gradient-button gradient-button"> Create</a>
-    <Button variant="outline-secondary">Wallet</Button>
-  </Navbar.Collapse>
-</Navbar>
-
-
-
-
-
-
-
-
-
-
-
-      {/* <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#" className="nav-logo">
-          <img src="assets/images/logo.png" height="40" width="48" alt="Barible" />
-          <b className="icon-name">Barible </b>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-1 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Creator, item or collection"
-                className="search-bar"
-                aria-label="Search"
+   
+     
+        <div className="main-nav">
+          
+          <Navbar bg="light" expand="lg" className="navbar-main11">
+         
+            <Navbar.Brand href="#" className="nav-logo">
+              <img
+                src="assets/images/logo.png"
+                height="40"
+                width="48"
+                alt="Barible"
               />
-            </Form>
+              <b className="icon-name">Barible </b>
+            </Navbar.Brand>
            
-              
-            <Nav.Link href="/" className="nav-expl">
-              <b>Explore </b>
-            </Nav.Link>
-            <Nav.Link href="/myitem" className="nav-myitem">
-              <b>My items </b>
-            </Nav.Link>
-            <Nav.Link href="/following" className="nav-foll">
-              <b>Following </b>
-            </Nav.Link>
-            <Nav.Link href="/activity" className="nav-act">
-              <b>Activity </b>
-            </Nav.Link>
-            <Nav.Link className="nav-line">|</Nav.Link>
-            <Nav.Link href="/howitworks" className="nav-howit">
-              <b>How it works </b>
-            </Nav.Link>
-            <NavDropdown
-              title="Community"
-              id="navbarScrollingDropdown"
-              className="nav-community"
-            >
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <a class="gradient-button gradient-button"> Create</a>
             
-            <Button variant="outline-secondary" className="btn-connect">
-              Connect wallet
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar> */}
-    </div>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            
+              <Nav
+             
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                
+                <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="collection, item or user"
+                    className="mr-2"
+                    aria-label="Search"
+                  />
+                </Form>
+                
+              
+                <Nav.Link href="/" className="nav-explore">
+                 
+                  <b>Explore </b>{" "}
+                </Nav.Link>
+         
+                <Nav.Link href="/myitem" className="nav-item">
+                  {" "}
+                  <b>items </b>{" "}
+                </Nav.Link>
+   
+                <Nav.Link href="/following" className="nav-following">
+                  {" "}
+                  <b>Following </b>{" "}
+                </Nav.Link>
+   
+                <Nav.Link href="/activity" className="nav-activity">
+                  {" "}
+                  <b>Activity </b>{" "}
+                </Nav.Link>
+                
+                <Nav.Link href="/howitworks" className="nav-howitwork">
+                  {" "}
+                  <b>Works </b>{" "}
+                </Nav.Link>
+  
+                <NavDropdown
+                  title="Community"
+                  className="nav-community"
+                  id="navbarScrollingDropdown"
+                >
+                  <NavDropdown.Item href="#action3">
+                    Rari Token
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Discussion
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Voting</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Suggest Feature
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Barible Protocol
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Subscribe</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <div className="icon-name">
+              <a
+                className="btn btn-social-icon btn-google"
+                href="http://google.com/+"
+              >
+                <i className="fa fa-google-plus"></i>
+              </a>
+              <a
+                className="btn btn-social-icon btn-facebook"
+                href="http://www.facebook.com/profile.php?id="
+              >
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a
+                className="btn btn-social-icon btn-google"
+                href="http://youtube.com/"
+              >
+                <i className="fa fa-youtube"></i>
+              </a>
+              <a
+                className="btn btn-social-icon btn-twitter"
+                href="http://twitter.com/"
+              >
+                <i className="fa fa-twitter"></i>
+              </a>
+              
+              
+            </div>
+                </NavDropdown>
+                
+              </Nav>
+              
+              <button class="btn-hover color">Create</button> 
+              
+              <Button variant="outline-secondary" className="nav-walletbtn"> 
+                Wallet
+              </Button>
+              
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+    
   );
 };
